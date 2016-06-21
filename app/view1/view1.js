84ca324e7b8933ab1bfff15d9a179921b47ac883
nika <skarlett-fantastka@yandex.ru>
2016-06-21 18:59:17 +0300

    v21_ev

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -3,12 +3,36 @@
 angular.module('myApp.view1', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
+    $routeProvider.when('/view1', {
     templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
+    controller: 'SelectProperty'
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.factory('defaultData', function(){
+    return {
+        chooseProperty: {test: 'fg'}
+    };
+})
+.controller('SelectProperty', ['$scope', 'defaultData', function($scope, defaultData) {
+    this.saveProperties = function(selectedProperties){
+        defaultData.properties = selectedProperties;
+        $scope.code =  selectedProperties;
+        console.log($scope);
+    };
+}])
+
+
+
+.controller('WriteCode', ['$scope', 'defaultData', function($scope, defaultData){
+
+   // $scope.codes =  $scope.code;
+    console.log( $scope.code);
+   /* $scope.$watch(
+        "code",
+        function handleFooChange( newValue, oldValue ) {
+            console.log( "WriteCode.code:", newValue );
+        }
+    );*/
 
 }]);
