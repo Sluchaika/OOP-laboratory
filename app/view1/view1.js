df8a6246bf48a5021db5a020aff5a26512ab4879
nika <skarlett-fantastka@yandex.ru>
2016-06-22 18:47:43 +0300

    v22

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -4,35 +4,118 @@ angular.module('myApp.view1', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'SelectProperty'
-  });
+        templateUrl: 'view1/view1.html',
+        controller: 'propertiesCtrl'
+    });
 }])
 
 .factory('defaultData', function(){
+    var Ball = {
+        color:'red'
+    };
+
+
     return {
-        chooseProperty: {test: 'fg'}
+       Ball: Ball
     };
 })
-.controller('SelectProperty', ['$scope', 'defaultData', function($scope, defaultData) {
-    this.saveProperties = function(selectedProperties){
-        defaultData.properties = selectedProperties;
-        $scope.code =  selectedProperties;
-        console.log($scope);
+
+.factory('codeService', ['defaultData', function(defaultData){
+    var code = '';
+    function add (str) {
+        code+=str;
+    }
+    function addProperties(props){
+        defaultData.Ball.prototype = props;
+        console.log(props);
+        code += 'Ball.prototype' + props;
+    }
+
+    return {
+        addProperties: addProperties,
+        objProperties: {
+            size:100,
+            color: 'red'},
+        objMethods: {}
     };
 }])
 
+.factory('draw', function(){
 
 
-.controller('WriteCode', ['$scope', 'defaultData', function($scope, defaultData){
+    return {
+        drawBall: function(elem, option){
 
-   // $scope.codes =  $scope.code;
-    console.log( $scope.code);
-   /* $scope.$watch(
-        "code",
-        function handleFooChange( newValue, oldValue ) {
-            console.log( "WriteCode.code:", newValue );
+            var ctx = elem[0].getContext('2d');
+            ctx.beginPath();
+            console.log(option.size);
+            ctx.arc(option.size, option.size, option.size/2, 0, Math.PI*2, false);
+            ctx.closePath();
+            ctx.fillStyle = option.color;
+            ctx.fill();
+            ctx = elem[0].getContext('2d');
+            ctx.beginPath();
+            console.log(option.size);
+            ctx.arc(200, 200, 100, 0, Math.PI*2, false);
+            ctx.closePath();
+            ctx.fillStyle = 'blue';
+            ctx.fill();
         }
-    );*/
+    };
+})
+
+.controller('propertiesCtrl', ['$scope', '$rootScope', 'codeService',  'defaultData', function($scope, $rootScope, codeService, defaultData) {
+    $scope.objProperty = codeService.objProperties;
+    this.saveProperties = function(selectedProperties){
+        codeService.objProperties = selectedProperties;
+        codeService.addProperties(selectedProperties);
+        $rootScope.properties = selectedProperties;
+
+    };
+
+}])
+
+.controller('methodsCtrl', ['$scope', '$rootScope', 'codeService', 'defaultData', function($scope, $rootScope, codeService, defaultData) {
+    this.saveMethods = function(selectedMethods){
+        codeService.objMethods= selectedMethods;
+
+        $rootScope.changes = true;
+    };
+
+}])
+
+
+.controller('drawCtrl', ['$scope', '$rootScope', '$element', 'codeService', 'draw', function($scope,  $rootScope, $element, codeService, draw){
+    //тут проверяем существование того или иного метода и добавляем фукнционал. Проверяем свойства, перерисовываем шар.
+    //нужно получить список свойсвт
+
+    $rootScope.$watch(
+        'properties',
+        function handleFooChange() {
+           /* for (var key in codeService.objProperties){
+                if (key === 'color') {*/
+            draw.drawBall($element, codeService.objProperties);
+        /*        }
+            }*/
+
+        }
+    );
 
 }]);
+
+/*.controller('WriteCode', ['$scope', '$rootScope', 'defaultData', 'codeService', function($scope,  $rootScope, defaultData, codeService){
+   //получить данные, выбранные в формах о свойствах, методах, создание новые объектов
+    //эти данные нужно добавить в объект и вывести код. Или же код вывести после уже готового всего?
+
+    $rootScope.$watch(
+        'properties',
+        function handleFooChange() {
+            for (var key in codeService.objProperties){
+                if (key === 'color'){
+
+                }
+            }
+        }
+    );
+
+}]);*/
